4f829d8cd647367bc0051ffdca328088893d4d32
JUSTINE KYLE SANTOS <justinekyle.santos.cvt@eac.edu.ph>
2026-09-10 10:53:49 +0800

    Student B authentication code

--- OneDrive/Desktop/se-team-workflowz-classmate/auth.js
+++ OneDrive/Desktop/se-team-workflowz-classmate/auth.js
@@ -1,12 +1,4 @@
 function login(username, password) {
-    if (!username || !password) {
-        return "Username and password are required";
-    }
-
-    if (username === "admin" && password === "1234") {
-        return "Login successful";
-    }
-
-    return "Invalid username or password";
+    return "Student B Refactored Code";
 }
 module.exports = { login };
